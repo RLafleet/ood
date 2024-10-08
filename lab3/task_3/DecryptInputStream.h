2743991ec858d6ce7f98d1b33175efa0e6f2bbfe
RLafleet <maksim.nazarov@ispring.institute>
2024-10-08 13:58:43 +0300

    Enc и Dec

---
+++ lab3/task_3/DecryptInputStream.h
@@ -0,0 +1,41 @@
+﻿#ifndef DECRYPTINPUTSTREAM_H
+#define DECRYPTINPUTSTREAM_H
+
+#include "IInputDataStream.h"
+#include <memory>
+#include <vector>
+
+class DecryptInputStream : public IInputDataStream
+{
+public:
+    DecryptInputStream(std::unique_ptr<IInputDataStream> stream, uint32_t key)
+        : m_stream(std::move(stream)), m_reverseTable(GenerateReverseSubstitutionTable(GenerateSubstitutionTable(key))) {}
+
+    uint8_t ReadByte() override
+    {
+        uint8_t data = m_stream->ReadByte();
+        return m_reverseTable[data];
+    }
+
+    std::streamsize ReadBlock(void* dstBuffer, std::streamsize size) override
+    {
+        std::streamsize bytesRead = m_stream->ReadBlock(dstBuffer, size);
+        uint8_t* bytes = static_cast<uint8_t*>(dstBuffer);
+        for (std::streamsize i = 0; i < bytesRead; ++i)
+        {
+            bytes[i] = m_reverseTable[bytes[i]];
+        }
+        return bytesRead;
+    }
+
+    bool IsEOF() const override
+    {
+        return m_stream->IsEOF();
+    }
+
+private:
+    std::unique_ptr<IInputDataStream> m_stream;
+    std::vector<uint8_t> m_reverseTable;
+};
+
+#endif // DECRYPTINPUTSTREAM_H
