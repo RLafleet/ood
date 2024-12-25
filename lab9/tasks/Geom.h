cc63d4bc004a98d0edbcb1962b262c59e1927d07
RLafleet <maksim.nazarov@ispring.institute>
2024-12-25 21:08:31 +0300

    geom

---
+++ lab9/tasks/Geom.h
@@ -0,0 +1,27 @@
+﻿#ifndef GEOM_H
+#define GEOM_H
+
+struct Point
+{
+	int x = 0;
+	int y = 0;
+};
+
+struct Size
+{
+	unsigned width = 0;
+	unsigned height = 0;
+
+	bool operator==(const Size& size) const
+	{
+		return size.width == width && size.height == height;
+	}
+};
+
+// Точка передаётся в локальных координатах.
+inline bool IsPointInSize(const Point p, const Size size) noexcept
+{
+	return static_cast<unsigned int>(p.x) < size.width && static_cast<unsigned int>(p.y) < size.height;
+}
+
+#endif //GEOM_H
