f284ef0e60c67398d6e2c49fad7feb91c89119c5
RLafleet <maksim.nazarov@ispring.institute>
2024-12-25 21:05:13 +0300

    quarter satte multi

---
+++ lab8/tasks/CHasQuarterStateMulti.h
@@ -0,0 +1,72 @@
+#ifndef CHASQUARTERSTATEMULTI_H
+#define CHASQUARTERSTATEMULTI_H
+
+#include <iostream>
+#include "IStateMulti.h"
+#include "IMultiGumballMachine.h"
+
+namespace multi_gumball_machine
+{
+	class CHasQuarterState final : public IState
+	{
+	public:
+		explicit CHasQuarterState(IMultiGumballMachine& gumballMachine)
+			: m_gumballMachine(gumballMachine)
+		{
+		}
+
+		void InsertQuarter() override
+		{
+			if (m_gumballMachine.GetQuarterCount() < m_gumballMachine.GetMaxQuarterCount())
+			{
+				m_gumballMachine.AddQuarter();
+				std::cout << "You inserted another quarter\n";
+			}
+			else
+			{
+				std::cout << "You can't insert another quarter\n";
+			}
+		}
+
+		void EjectQuarter() override
+		{
+			m_gumballMachine.ReturnAllQuarters();
+			m_gumballMachine.SetNoQuarterState();
+		}
+
+		void TurnCrank() override
+		{
+			if (m_gumballMachine.GetQuarterCount() > 0)
+			{
+				std::cout << "You turned...\n";
+				m_gumballMachine.SetSoldState();
+			}
+			else
+			{
+				std::cout << "No quarters left.\n";
+				m_gumballMachine.SetNoQuarterState();
+			}
+		}
+
+		void Dispense() override
+		{
+			std::cout << "No gumball dispensed\n";
+		}
+
+		void Refill(const unsigned numBalls) override
+		{
+			std::cout << "Added gumball\n";
+			m_gumballMachine.RefillBall(numBalls);
+		}
+
+		[[nodiscard]] std::string ToString() const override
+		{
+			return "waiting for turn of crank";
+		}
+
+	private:
+		IMultiGumballMachine& m_gumballMachine;
+	};
+}
+
+#endif //CHASQUARTERSTATEMULTI_H
