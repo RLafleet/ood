c11d982933c2f01d85d8955a5abf3dd0e42d3f9b
RLafleet <maksim.nazarov@ispring.institute>
2024-12-25 21:07:40 +0300

    init

---
+++ lab8/tasks/tasks.cpp
@@ -0,0 +1,12 @@
+#include <memory>
+#include "CommandHandler.h"
+#include "CMultiGumballMachine.h"
+
+int main()
+{
+	auto machine = std::make_unique<multi_gumball_machine::CMultiGumballMachine>(0);
+	CommandHandler handler(std::move(machine));
+	handler.Run();
+
+	return 0;
+}
