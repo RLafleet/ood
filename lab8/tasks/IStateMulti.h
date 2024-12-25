9ddd4c087002e1f5cb8fc303db66ff7e3723f843
RLafleet <maksim.nazarov@ispring.institute>
2024-12-25 21:07:33 +0300

    state multi

---
+++ lab8/tasks/IStateMulti.h
@@ -0,0 +1,26 @@
+#ifndef ISTATEMULTI_H
+#define ISTATEMULTI_H
+
+#include <string>
+
+namespace multi_gumball_machine
+{
+	struct IState
+	{
+		virtual void InsertQuarter() = 0;
+
+		virtual void EjectQuarter() = 0;
+
+		virtual void TurnCrank() = 0;
+
+		virtual void Dispense() = 0;
+
+		virtual void Refill(unsigned numBalls) = 0;
+
+		[[nodiscard]] virtual std::string ToString() const = 0;
+
+		virtual ~IState() = default;
+	};
+}
+
+#endif //ISTATEMULTI_H
